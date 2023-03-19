ed44b41b9c496fdc82b8724651892e99e589a40e
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-19 11:37:18 +0200

    UPDATE

---
+++ DirectXTKTest/WinMainHelper.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// #define DATE
+// extern "C"
+// {
+//     __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+//     __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+// }
