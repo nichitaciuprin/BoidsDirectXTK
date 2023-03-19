73f2a4702fe364d8c49c462b46422fb2e45c7237
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-19 22:58:26 +0200

    UPDATE

--- DirectXTKTest/src/Game.cpp
+++ DirectXTKTest/src/Game.cpp
@@ -1,45 +1,22 @@
-//
-// Game.cpp
-//
-
 #include "pch.h"
 #include "Game.h"
-
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
-
 using Microsoft::WRL::ComPtr;
 
 Game::Game()
 {
     m_deviceResources = std::make_unique<DX::DeviceResources>();
-    // TODO: Provide parameters for swapchain format, depth/stencil format, and backbuffer count.
-    //   Add DX::DeviceResources::c_AllowTearing to opt-in to variable rate displays.
-    //   Add DX::DeviceResources::c_EnableHDR for HDR10 display.
     m_deviceResources->RegisterDeviceNotify(this);
 }
-
-// Initialize the Direct3D resources required to run.
 void Game::Initialize(HWND window, int width, int height)
 {
     m_deviceResources->SetWindow(window, width, height);
-
     m_deviceResources->CreateDeviceResources();
     CreateDeviceDependentResources();
-
     m_deviceResources->CreateWindowSizeDependentResources();
     CreateWindowSizeDependentResources();
-
-    // TODO: Change the timer settings if you want something other than the default variable timestep mode.
-    // e.g. for 60 FPS fixed timestep update logic, call:
-    /*
-    m_timer.SetFixedTimeStep(true);
-    m_timer.SetTargetElapsedSeconds(1.0 / 60);
-    */
 }
-
-#pragma region Frame Update
-// Executes the basic game loop.
 void Game::Tick()
 {
     m_timer.Tick([&]()
@@ -49,19 +26,10 @@ void Game::Tick()
 
     Render();
 }
-
-// Updates the world.
 void Game::Update(DX::StepTimer const& timer)
 {
     float elapsedTime = float(timer.GetElapsedSeconds());
-
-    // TODO: Add your game logic here.
-    elapsedTime;
 }
-#pragma endregion
-
-#pragma region Frame Render
-// Draws the scene.
 void Game::Render()
 {
     // Don't try to render anything before the first Update.
@@ -83,7 +51,6 @@ void Game::Render()
     // Show the new frame.
     m_deviceResources->Present();
 }
-
 // Helper method to clear the back buffers.
 void Game::Clear()
 {
@@ -104,43 +71,32 @@ void Game::Clear()
 
     m_deviceResources->PIXEndEvent();
 }
-#pragma endregion
-
-#pragma region Message Handlers
-// Message handlers
 void Game::OnActivated()
 {
     // TODO: Game is becoming active window.
 }
-
 void Game::OnDeactivated()
 {
     // TODO: Game is becoming background window.
 }
-
 void Game::OnSuspending()
 {
     // TODO: Game is being power-suspended (or minimized).
 }
-
 void Game::OnResuming()
 {
     m_timer.ResetElapsedTime();
-
     // TODO: Game is being power-resumed (or returning from minimize).
 }
-
 void Game::OnWindowMoved()
 {
     auto const r = m_deviceResources->GetOutputSize();
     m_deviceResources->WindowSizeChanged(r.right, r.bottom);
 }
-
 void Game::OnDisplayChange()
 {
     m_deviceResources->UpdateColorSpace();
 }
-
 void Game::OnWindowSizeChanged(int width, int height)
 {
     if (!m_deviceResources->WindowSizeChanged(width, height))
@@ -150,17 +106,12 @@ void Game::OnWindowSizeChanged(int width, int height)
 
     // TODO: Game window is being resized.
 }
-
-// Properties
 void Game::GetDefaultSize(int& width, int& height) const noexcept
 {
     // TODO: Change to desired default window size (note minimum size is 320x200).
     width = 800;
     height = 600;
 }
-#pragma endregion
-
-#pragma region Direct3D Resources
 // These are the resources that depend on the device.
 void Game::CreateDeviceDependentResources()
 {
@@ -169,22 +120,18 @@ void Game::CreateDeviceDependentResources()
     // TODO: Initialize device dependent objects here (independent of window size).
     device;
 }
-
 // Allocate all memory resources that change on a window SizeChanged event.
 void Game::CreateWindowSizeDependentResources()
 {
     // TODO: Initialize windows-size dependent objects here.
 }
-
 void Game::OnDeviceLost()
 {
     // TODO: Add Direct3D resource cleanup here.
 }
-
 void Game::OnDeviceRestored()
 {
     CreateDeviceDependentResources();
 
     CreateWindowSizeDependentResources();
 }
-#pragma endregion
