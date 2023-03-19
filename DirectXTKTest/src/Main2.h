084b0dd821212f9e23680ed4b7b5e9ede8492728
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-19 16:46:07 +0200

    UPDATE

--- DirectXTKTest/src/Main2.h
+++ DirectXTKTest/src/Main2.h
@@ -1,19 +1,19 @@
 #include "pch.h"
-// #include "Game.h"
-// #include "WinMainWindow.h"
-// using namespace std;
+#include "Game.h"
+#include "WinMainWindow.h"
+using namespace std;
 
 void wWinMain2(HINSTANCE hInstance, int nCmdShow)
 {
-    // auto game = make_unique<Game>();
-    // if (WinMainWindow_Create(hInstance,nCmdShow,game.get())) return;
-    // while(true)
-    // {
-    //     if (WinMainWindow_ShouldQuit()) break;
+    auto game = make_unique<Game>();
+    if (WinMainWindow_Create(hInstance,nCmdShow,game.get())) return;
+    while(true)
+    {
+        if (WinMainWindow_ShouldQuit()) break;
 
-    //     // Game logic must be here
+        // Game logic must be here
 
-    //     WinMainWindow_Render(game.get());
-    // }
-    // game.reset();
+        WinMainWindow_Render(game.get());
+    }
+    game.reset();
 }
