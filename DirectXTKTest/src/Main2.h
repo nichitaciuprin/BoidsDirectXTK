7a13675d4f9990435b7b51a068a2a06b6e28330e
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-19 16:41:49 +0200

    TEMP

---
+++ DirectXTKTest/src/Main2.h
@@ -0,0 +1,19 @@
+#include "pch.h"
+// #include "Game.h"
+// #include "WinMainWindow.h"
+// using namespace std;
+
+void wWinMain2(HINSTANCE hInstance, int nCmdShow)
+{
+    // auto game = make_unique<Game>();
+    // if (WinMainWindow_Create(hInstance,nCmdShow,game.get())) return;
+    // while(true)
+    // {
+    //     if (WinMainWindow_ShouldQuit()) break;
+
+    //     // Game logic must be here
+
+    //     WinMainWindow_Render(game.get());
+    // }
+    // game.reset();
+}
