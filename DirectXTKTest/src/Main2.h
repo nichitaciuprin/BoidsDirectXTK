9ee988240cc8e56c424e315fa951ecc9e14afae6
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-21 23:43:03 +0200

    UPDATE

--- DirectXTKTest/src/Main2.h
+++ DirectXTKTest/src/Main2.h
@@ -2,11 +2,14 @@
 #include "pch.h"
 #include "World.h"
 #include "WorldWindow.h"
+#include "console.h"
 
 void wWinMain2(HINSTANCE hInstance)
 {
     auto world = make_unique<World>();
     auto worldWindow = make_unique<WorldWindow>(hInstance);
+
+    Console_Test();
 
     while(true)
     {
---
+++ DirectXTKTest/src/console.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+// #include <stdio.h>
+
+void Console_Test()
+{
+    AllocConsole();
+    auto consoleHwnd = GetConsoleWindow();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+    std::cout << "Hello\n";
+    SetWindowPos(consoleHwnd,0,2000,0,0,0,SWP_NOSIZE);
+}
