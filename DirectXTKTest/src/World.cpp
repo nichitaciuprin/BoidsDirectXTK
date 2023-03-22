43be6d2d0f15550ec4fcbf03cf39426af29ae8b9
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-22 22:20:11 +0200

    UPDATE

--- DirectXTKTest/src/World.cpp
+++ DirectXTKTest/src/World.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "pch.h"
 #include "World.h"
+#include <Windows.h>
 using namespace DirectX::SimpleMath;
 
 World::World()
@@ -8,5 +9,9 @@ World::World()
 }
 void World::Update(float deltaTime)
 {
-    cameraPosition += Vector3::Right*deltaTime;
+    if(GetKeyState(VK_SHIFT) & 0x8000)
+    {
+        // https://stackoverflow.com/questions/41600981/how-do-i-check-if-a-key-is-pressed-on-c
+        cameraPosition += Vector3::Right*deltaTime;
+    }
 }
