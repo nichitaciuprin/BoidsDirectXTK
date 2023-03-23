b16429794d338e674915c81a99557de1c380b394
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-23 17:31:51 +0200

    UPDATE

--- DirectXTKTest/src/Helper.h
+++ DirectXTKTest/src/Helper.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <Windows.h>
+
+class Helper
+{
+public:
+    static bool EscapePressed()
+    {
+        // https://stackoverflow.com/questions/41600981/how-do-i-check-if-a-key-is-pressed-on-c
+        auto keyState = GetKeyState(VK_ESCAPE);
+        auto pressed = keyState & 0x8000;
+        // auto toggled = keyState & 0x0001;
+        return pressed;
+    }
+};
--- DirectXTKTest/src/Main2.h
+++ DirectXTKTest/src/Main2.h
@@ -2,14 +2,16 @@
 #include "pch.h"
 #include "World.h"
 #include "WorldWindow.h"
+#include "Helper.h"
 #include "console.h"
 
 void wWinMain2(HINSTANCE hInstance)
 {
     auto world = make_unique<World>();
     auto worldWindow = make_unique<WorldWindow>(hInstance);
-    while(!worldWindow->quit)
+    while(true)
     {
+        if (Helper::EscapePressed()) break;
         auto directionWASD = worldWindow->DirectionWASD();
         world->Update(0.02f,directionWASD,Vector2::Zero);
         worldWindow->Render(world.get());
