f937a076d794cadb3ca40b5d0772b6ed89586b31
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-23 17:50:47 +0200

    UPDATE

--- DirectXTKTest/src/World.h
+++ DirectXTKTest/src/World.h
@@ -12,16 +12,6 @@ public:
     }
     void Update(float deltaTime, Vector2 wasdDirection, Vector2 mouseRotation)
     {
-        // SHORT state = GetKeyState(VK_SHIFT);
-        // SHORT state2 = GetKeyState(VK_SHIFT) & 0x8000;
-        // SHORT test = 0x0001;
-        // auto isHightOrderBit = (GetKeyState(VK_SHIFT) >> 15);
-        // if (GetKeyState(VK_SHIFT) & 0x8000)
-        // if(isHightOrderBit)
-        // {
-        //     // https://stackoverflow.com/questions/41600981/how-do-i-check-if-a-key-is-pressed-on-c
-        //     cameraPosition += Vector3::Right*deltaTime;
-        // }
         cameraPosition += Vector3(wasdDirection.x,wasdDirection.y,0) * deltaTime;
     }
 private:
