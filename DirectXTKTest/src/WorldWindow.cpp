5ca2f2c2f70f4675f7f471cd0e77332e493194ad
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-23 22:34:22 +0200

    UPDATE

--- DirectXTKTest/src/WorldWindow.cpp
+++ DirectXTKTest/src/WorldWindow.cpp
@@ -206,18 +206,7 @@ WorldWindow::WorldWindow(HINSTANCE hInstance)
     auto context = m_deviceResources->GetD3DDeviceContext();
     m_shape = GeometricPrimitive::CreateSphere(context);
 }
-Vector2 WorldWindow::DirectionWASD()
-{
-    auto value_1 = key_w ? 1 : 0;
-    auto value_2 = key_a ? -1 : 0;
-    auto value_3 = key_s ? -1 : 0;
-    auto value_4 = key_d ? 1 : 0;
-    auto axisY = value_3 + value_1;
-    auto axisX = value_2 + value_4;
-    Vector2 result = Vector2(axisX, axisY);
-    result.Normalize();
-    return result;
-}
+
 void WorldWindow::Render(const World* world)
 {
     while (PeekMessage(&msg, m_hwnd, 0, 0, PM_REMOVE))
@@ -247,25 +236,7 @@ void WorldWindow::Paint(const World* world)
     m_deviceResources->PIXEndEvent();
     m_deviceResources->Present();
 }
-void WorldWindow::Clear()
-{
-    m_deviceResources->PIXBeginEvent(L"Clear");
 
-    // Clear the views.
-    auto context = m_deviceResources->GetD3DDeviceContext();
-    auto renderTarget = m_deviceResources->GetRenderTargetView();
-    auto depthStencil = m_deviceResources->GetDepthStencilView();
-
-    context->ClearRenderTargetView(renderTarget, Colors::CornflowerBlue);
-    context->ClearDepthStencilView(depthStencil, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
-    context->OMSetRenderTargets(1, &renderTarget, depthStencil);
-
-    // Set the viewport.
-    auto const viewport = m_deviceResources->GetScreenViewport();
-    context->RSSetViewports(1, &viewport);
-
-    m_deviceResources->PIXEndEvent();
-}
 void WorldWindow::OnActivated()
 {
     // TODO: Game is becoming active window.
--- DirectXTKTest/src/WorldWindow.h
+++ DirectXTKTest/src/WorldWindow.h
@@ -2,6 +2,10 @@
 #include "pch.h"
 #include "World.h"
 #include "DeviceResources.h"
+#include "WorldWindow.h"
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+using Microsoft::WRL::ComPtr;
 
 using namespace DX;
 
@@ -25,11 +29,39 @@ public:
     bool key_a = false;
     bool key_s = false;
     bool key_d = false;
-    Vector2 DirectionWASD();
+    Vector2 DirectionWASD()
+    {
+        auto value_1 = key_w ? 1 : 0;
+        auto value_2 = key_a ? -1 : 0;
+        auto value_3 = key_s ? -1 : 0;
+        auto value_4 = key_d ? 1 : 0;
+        auto axisY = value_3 + value_1;
+        auto axisX = value_2 + value_4;
+        Vector2 result = Vector2(axisX, axisY);
+        result.Normalize();
+        return result;
+    }
 private:
-    void Clear();
+    void Clear()
+    {
+        m_deviceResources->PIXBeginEvent(L"Clear");
+
+        // Clear the views.
+        auto context = m_deviceResources->GetD3DDeviceContext();
+        auto renderTarget = m_deviceResources->GetRenderTargetView();
+        auto depthStencil = m_deviceResources->GetDepthStencilView();
+
+        context->ClearRenderTargetView(renderTarget, Colors::CornflowerBlue);
+        context->ClearDepthStencilView(depthStencil, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+        context->OMSetRenderTargets(1, &renderTarget, depthStencil);
+
+        // Set the viewport.
+        auto const viewport = m_deviceResources->GetScreenViewport();
+        context->RSSetViewports(1, &viewport);
+
+        m_deviceResources->PIXEndEvent();
+    }
     void Paint(const World* world);
-    void CreateDeviceDependentResources();
     void CreateWindowSizeDependentResources();
     unique_ptr<DeviceResources> m_deviceResources;
     HWND m_hwnd;
