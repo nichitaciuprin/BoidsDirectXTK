714629788edbdfac85ccf78d66619d37ea9f3459
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-28 09:35:10 +0300

    UPDATE

--- DirectXTKTest/src/Main2.h
+++ DirectXTKTest/src/Main2.h
@@ -8,16 +8,19 @@ void wWinMain2(HINSTANCE hInstance)
     auto world = make_unique<World>();
     MyWindow2::Create(hInstance);
 
+    const long timeStepInMilliseconds = 20;
+    const float timeStepInSeconds = 0.02f;
+
     while(true)
     {
         if (Helper::EscapePressed()) break;
 
         auto oldTime = Helper::GetTime();
 
-        world->Update(0.02f,MyWindow2::DirectionWASD(),Vector2::Zero);
+        world->Update(timeStepInSeconds,MyWindow2::DirectionWASD(),Vector2::Zero);
         MyWindow2::Render(world.get());
 
         auto newTime = Helper::GetTime();
-        Helper::Wait(oldTime,newTime,20);
+        Helper::Wait(oldTime,newTime,timeStepInMilliseconds);
     }
 }
