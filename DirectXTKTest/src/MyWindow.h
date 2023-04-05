1b7be7d5df7a2b6ca7ae0331bdb391819d92c82e
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-04-05 18:02:59 +0300

    UPDATE

--- DirectXTKTest/src/MyWindow.h
+++ DirectXTKTest/src/MyWindow.h
@@ -11,6 +11,220 @@ using namespace DirectX;
 using namespace DirectX::SimpleMath;
 using Microsoft::WRL::ComPtr;
 
+class Test
+{
+public:
+    static Vector2 DirectionWASD()
+    {
+        auto axisY = key_w + key_s;
+        auto axisX = key_a + key_d;
+        auto result = Vector2(axisX, axisY);
+        result.Normalize();
+        return result;
+    }
+private:
+    static const int defaultWidth = 800;
+    static const int defaultHeight = 600;
+    static bool classRegistered;
+    static bool sizemove;
+    static bool minimized;
+    static bool fullscreen;
+    static bool windowClosed;
+    static float key_w;
+    static float key_a;
+    static float key_s;
+    static float key_d;
+    static unique_ptr<DeviceResources> m_deviceResources;
+    static HWND m_hwnd;
+    static MSG msg;
+    static unique_ptr<GeometricPrimitive> m_shape;
+    static unique_ptr<Mouse> mouse;
+    static void Init()
+    {
+        msg = {};
+    }
+    static LPCWSTR ClassName() { return L"WorldWindow"; }
+    static LPCWSTR WindowName() { return L"WorldWindow"; }
+    static LPCWSTR IconName() { return L"IDI_ICON"; }
+    static void OnDisplayChange()
+    {
+        m_deviceResources->UpdateColorSpace();
+    }
+    static void OnWindowMoved()
+    {
+        auto outputSize = m_deviceResources->GetOutputSize();
+        m_deviceResources->WindowSizeChanged(outputSize.right, outputSize.bottom);
+    }
+    static void OnWindowSizeChanged(int width, int height)
+    {
+        if (!m_deviceResources->WindowSizeChanged(width, height))
+            return;
+    }
+    static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+    {
+        if (m_deviceResources == NULL)
+            return DefWindowProc(hwnd, message, wParam, lParam);
+
+        switch (message)
+        {
+            case WM_MOUSEACTIVATE: return MA_ACTIVATEANDEAT;
+            case WM_ACTIVATE:
+            case WM_ACTIVATEAPP:
+            case WM_INPUT:
+            case WM_MOUSEMOVE:
+            case WM_LBUTTONDOWN:
+            case WM_LBUTTONUP:
+            case WM_RBUTTONDOWN:
+            case WM_RBUTTONUP:
+            case WM_MBUTTONDOWN:
+            case WM_MBUTTONUP:
+            case WM_MOUSEWHEEL:
+            case WM_XBUTTONDOWN:
+            case WM_XBUTTONUP:
+            case WM_MOUSEHOVER:
+                Mouse::ProcessMessage(message, wParam, lParam);
+                break;
+            // case WM_SETCURSOR:
+            //     if (LOWORD(lParam) == HTCLIENT)
+            //     {
+            //         RECT clientRect;
+            //         RECT windowRect;
+            //         GetClientRect(hwnd, &clientRect);
+            //         GetWindowRect(hwnd, &windowRect);
+            //         AdjustWindowRect(&clientRect, WS_OVERLAPPEDWINDOW, FALSE);
+            //         RECT finalRect = { windowRect.left, windowRect.top, windowRect.left+clientRect.right, windowRect.top+clientRect.bottom };
+            //         ClipCursor(&windowRect);
+            //         ShowCursor(false);
+            //         // SetCursorPos(300,300);
+            //     }
+            //     break;
+            case WM_DISPLAYCHANGE:
+                OnDisplayChange();
+                break;
+            case WM_MOVE:
+                OnWindowMoved();
+                break;
+            case WM_SIZE:
+                if (wParam == SIZE_MINIMIZED && !minimized)
+                {
+                    minimized = true;
+                }
+                else if (minimized)
+                {
+                    minimized = false;
+                }
+                else if (!sizemove)
+                {
+                    OnWindowSizeChanged(LOWORD(lParam), HIWORD(lParam));
+                }
+                break;
+            case WM_EXITSIZEMOVE:
+                sizemove = false;
+                RECT rc;
+                GetClientRect(hwnd, &rc);
+                OnWindowSizeChanged(rc.right - rc.left, rc.bottom - rc.top);
+                break;
+            case WM_DESTROY:
+                windowClosed = true;
+                break;
+            case WM_SYSKEYDOWN:
+                switch (wParam)
+                {
+                    case VK_RETURN: // Alt+Enter
+                        if (fullscreen)
+                        {
+                            SetWindowLongPtr(hwnd, GWL_STYLE, WS_OVERLAPPEDWINDOW);
+                            SetWindowLongPtr(hwnd, GWL_EXSTYLE, 0);
+                            auto uFlags = SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED;
+                            SetWindowPos(hwnd, HWND_TOP, 0, 0, defaultWidth, defaultHeight, uFlags);
+                            ShowWindow(hwnd, SW_SHOWNORMAL);
+                        }
+                        else
+                        {
+                            SetWindowLongPtr(hwnd, GWL_STYLE, WS_POPUP);
+                            SetWindowLongPtr(hwnd, GWL_EXSTYLE, WS_EX_TOPMOST);
+                            auto uFlags = SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED;
+                            SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, uFlags);
+                            ShowWindow(hwnd, SW_SHOWMAXIMIZED);
+                        }
+                        fullscreen = !fullscreen;
+                    default: break;
+                }
+                break;
+            case WM_MENUCHAR:
+                // Removes error sound during alt+enter
+                // A menu is active and the user presses a key that does not correspond
+                // to any mnemonic or accelerator key
+                return MAKELRESULT(0, MNC_CLOSE);
+            case WM_KEYDOWN:
+                switch (wParam)
+                {
+                    case VK_ESCAPE: PostQuitMessage(0); break;
+                    case 'W': key_w =  1.0f; break;
+                    case 'A': key_a = -1.0f; break;
+                    case 'S': key_s = -1.0f; break;
+                    case 'D': key_d =  1.0f; break;
+                    default: break;
+                }
+                break;
+            case WM_KEYUP:
+                switch (wParam)
+                {
+                    case 'W': key_w = 0.0f; break;
+                    case 'A': key_a = 0.0f; break;
+                    case 'S': key_s = 0.0f; break;
+                    case 'D': key_d = 0.0f; break;
+                    default: break;
+                }
+                break;
+        }
+
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+    static void MaybeRegisterClass(HINSTANCE hInstance)
+    {
+        if (classRegistered) return;
+        WNDCLASSEXW windowClass = {};
+        windowClass.cbSize = sizeof(WNDCLASSEXW);
+        windowClass.style = CS_HREDRAW | CS_VREDRAW;
+        windowClass.lpfnWndProc = WndProc;
+        windowClass.hInstance = hInstance;
+        windowClass.hIcon = LoadIconW(hInstance, IconName());
+        windowClass.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+        windowClass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+        windowClass.lpszClassName = ClassName();
+        windowClass.hIconSm = LoadIconW(windowClass.hInstance, IconName());
+        if (!RegisterClassExW(&windowClass)) throw;
+        classRegistered = true;
+    }
+    static void Create(HINSTANCE hInstance)
+    {
+        MaybeRegisterClass(hInstance);
+
+        RECT rc = { 0, 0, static_cast<LONG>(defaultWidth), static_cast<LONG>(defaultHeight) };
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+        auto width = rc.right - rc.left;
+        auto height = rc.bottom - rc.top;
+        m_hwnd = CreateWindowExW(0 /*WS_EX_TOPMOST*/, ClassName(), WindowName(), WS_OVERLAPPEDWINDOW,
+            0, 0, width, height, nullptr, nullptr, hInstance, nullptr);
+        if (!m_hwnd) throw;
+        ShowWindow(m_hwnd, SW_SHOWNORMAL);
+        GetClientRect(m_hwnd, &rc);
+
+        mouse = make_unique<Mouse>();
+        mouse->SetWindow(m_hwnd);
+        mouse->SetMode(Mouse::MODE_RELATIVE);
+
+        m_deviceResources = make_unique<DeviceResources>();
+        m_deviceResources->SetWindow(m_hwnd, width, height);
+        m_deviceResources->CreateDeviceResources();
+        m_deviceResources->CreateWindowSizeDependentResources();
+        auto context = m_deviceResources->GetD3DDeviceContext();
+        m_shape = GeometricPrimitive::CreateSphere(context);
+    }
+};
+
 namespace MyWindow
 {
     const LPCWSTR className = L"WorldWindow";
