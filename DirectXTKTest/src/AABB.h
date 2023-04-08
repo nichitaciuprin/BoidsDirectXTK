ad27e9cebcc2aea135ec9d862d46fc1a037dcae3
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-04-08 17:05:53 +0300

    Fixed AABB

--- DirectXTKTest/src/AABB.h
+++ DirectXTKTest/src/AABB.h
@@ -9,13 +9,16 @@ struct AABB
     Vector3 p1;
     float MinX() { return fminf(p0.x,p1.x); }
     float MinY() { return fminf(p0.y,p1.y); }
+    float MinZ() { return fminf(p0.z,p1.z); }
     float MaxX() { return fmaxf(p0.x,p1.x); }
     float MaxY() { return fmaxf(p0.y,p1.y); }
+    float MaxZ() { return fmaxf(p0.z,p1.z); }
     Vector3 Size()
     {
         Vector3 result = p0-p1;
         result.x = fabs(result.x);
         result.y = fabs(result.y);
+        result.z = fabs(result.z);
         return result;
     }
 };
