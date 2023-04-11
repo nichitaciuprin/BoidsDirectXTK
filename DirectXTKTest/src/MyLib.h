ba3d08a75a0eb93c6805209b326942c5978eeb2e
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-04-11 15:01:30 +0300

    Fixed missing StartMyLib and EndMyLib

--- DirectXTKTest/src/MyLib.h
+++ DirectXTKTest/src/MyLib.h
@@ -7,6 +7,24 @@
 #include "World.h"
 #include <time.h>
 
+int StartMyLib()                            // Must be called before this library use
+{
+    // asserts that DirectXMath can be used
+    if (!DirectX::XMVerifyCPUSupport()) return 1;
+
+    // Initializes the COM library for use by the calling thread, sets the thread's concurrency model,
+    // and creates a new apartment for the thread if one is required.
+    if (FAILED(CoInitializeEx(nullptr, COINITBASE_MULTITHREADED))) return 1;
+
+    return 0;
+}
+void EndMyLib()                             // Must be called after  this library use
+{
+    // Closes the COM library on the current thread, unloads all DLLs loaded by the thread,
+    // frees any other resources that the thread maintains, and forces all RPC connections on the thread to close.
+    CoUninitialize();
+}
+
 long GetTime()                                            // Returns curent time in milliseconds
 {
     return clock();
