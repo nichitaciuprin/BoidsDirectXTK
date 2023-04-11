001950d4903653a2310397bca749f2df38360487
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-04-11 12:38:28 +0300

    Moved Main2.h to Main.cpp

--- DirectXTKTest/src/Main.cpp
+++ DirectXTKTest/src/Main.cpp
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include <DirectXMath.h>
-#include "Main2.h"
+#include "Helper.h"
+#include "World.h"
+#include "Window.h"
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
@@ -16,7 +18,24 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     // and creates a new apartment for the thread if one is required.
     if (FAILED(CoInitializeEx(nullptr, COINITBASE_MULTITHREADED))) return 1;
 
-    wWinMain2(hInstance);
+    const long timeStepInMilliseconds = 10;
+    const float timeStepInSeconds = 0.020f;
+
+    auto world = make_unique<World>();
+    Window::Create(hInstance);
+
+    while(true)
+    {
+        if (Helper::EscapePressed()) break;
+
+        auto oldTime = Helper::GetTime();
+
+        world->Update(timeStepInSeconds,Window::DirectionWASD(),Window::MouseLook());
+        Window::Render(world.get());
+
+        auto newTime = Helper::GetTime();
+        Helper::Wait(oldTime,newTime,timeStepInMilliseconds);
+    }
 
     // Closes the COM library on the current thread, unloads all DLLs loaded by the thread,
     // frees any other resources that the thread maintains, and forces all RPC connections on the thread to close.
