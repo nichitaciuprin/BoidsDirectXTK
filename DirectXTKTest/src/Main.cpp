6914cdec80680f0008e39f868c735500c11ce90e
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-04-11 13:48:26 +0300

    Moved start and end code to MyLib.h

--- DirectXTKTest/src/Main.cpp
+++ DirectXTKTest/src/Main.cpp
@@ -1,8 +1,5 @@
 #include <Windows.h>
-#include <DirectXMath.h>
-#include "Helper.h"
-#include "World.h"
-#include "Window.h"
+#include "MyLib.h"
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
@@ -11,12 +8,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     UNREFERENCED_PARAMETER(lpCmdLine);
     UNREFERENCED_PARAMETER(nCmdShow);
 
-    // asserts that DirectXMath can be used
-    if (!DirectX::XMVerifyCPUSupport()) return 1;
-
-    // Initializes the COM library for use by the calling thread, sets the thread's concurrency model,
-    // and creates a new apartment for the thread if one is required.
-    if (FAILED(CoInitializeEx(nullptr, COINITBASE_MULTITHREADED))) return 1;
+    if (StartMyLib()) return 1;
 
     const long timeStepInMilliseconds = 10;
     const float timeStepInSeconds = 0.020f;
@@ -37,9 +29,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         Helper::Wait(oldTime,newTime,timeStepInMilliseconds);
     }
 
-    // Closes the COM library on the current thread, unloads all DLLs loaded by the thread,
-    // frees any other resources that the thread maintains, and forces all RPC connections on the thread to close.
-    CoUninitialize();
+    EndMyLib();
 
     return 0;
 }
--- DirectXTKTest/src/MyLib.h
+++ DirectXTKTest/src/MyLib.h
@@ -1,2 +1,25 @@
 #pragma once
 
+#include <Windows.h>
+#include <DirectXMath.h>
+#include "Window.h"
+#include "Helper.h"
+#include "World.h"
+
+int StartMyLib()
+{
+    // asserts that DirectXMath can be used
+    if (!DirectX::XMVerifyCPUSupport()) return 1;
+
+    // Initializes the COM library for use by the calling thread, sets the thread's concurrency model,
+    // and creates a new apartment for the thread if one is required.
+    if (FAILED(CoInitializeEx(nullptr, COINITBASE_MULTITHREADED))) return 1;
+
+    return 0;
+}
+void EndMyLib()
+{
+    // Closes the COM library on the current thread, unloads all DLLs loaded by the thread,
+    // frees any other resources that the thread maintains, and forces all RPC connections on the thread to close.
+    CoUninitialize();
+}
