20f295a766b03e3ac5450a0698342588fc88760d
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-04-12 16:35:15 +0300

    UPDATE

--- DirectXTKWrapper/Console.h
+++ DirectXTKWrapper/Console.h
@@ -3,42 +3,41 @@
 #include <iostream>
 using namespace std;
 
-FILE* fileIn;
-FILE* fileOut;
-FILE* fileErr;
-
-bool consoleCreated = false;
-void Console_MaybeCreate()
+class Console
 {
-    if (consoleCreated) return;
-    auto curentWindow = GetActiveWindow();
-    AllocConsole();
-    SetFocus(curentWindow);
-    auto consoleWindow = GetConsoleWindow();
-    errno_t result;
-    result = freopen_s(&fileIn ,"CONIN$" ,"r",stdin ); if (result != 0) throw;
-    result = freopen_s(&fileOut,"CONOUT$","w",stdout); if (result != 0) throw;
-    result = freopen_s(&fileErr,"CONOUT$","w",stderr); if (result != 0) throw;
-    SetWindowPos(consoleWindow,0,2000,0,0,0,SWP_NOSIZE);
-    consoleCreated = true;
-}
-void Console_Write(int msg)
-{
-    Console_MaybeCreate();
-    std::cout << msg;
-}
-void Console_WriteLineInt(int msg)
-{
-    Console_MaybeCreate();
-    std::cout << msg << '\n';
-}
-void Console_WriteLineLong(long msg)
-{
-    Console_MaybeCreate();
-    std::cout << msg << '\n';
-}
-void Console_WriteLine(string msg)
-{
-    Console_MaybeCreate();
-    std::cout << msg << '\n';
-}
+public:
+    static void WriteLine(string msg)
+    {
+        Init();
+        std::cout << msg << '\n';
+    }
+    static void WriteLineInt(int msg)
+    {
+        Init();
+        std::cout << msg << '\n';
+    }
+    static void WriteLineLong(long msg)
+    {
+        Init();
+        std::cout << msg << '\n';
+    }
+private:
+    static void Init()
+    {
+        if (consoleCreated) return;
+        auto curentWindow = GetActiveWindow();
+        AllocConsole();
+        SetFocus(curentWindow);
+        auto consoleWindow = GetConsoleWindow();
+        errno_t result;
+        result = freopen_s(&fileIn ,"CONIN$" ,"r",stdin ); if (result != 0) throw;
+        result = freopen_s(&fileOut,"CONOUT$","w",stdout); if (result != 0) throw;
+        result = freopen_s(&fileErr,"CONOUT$","w",stderr); if (result != 0) throw;
+        SetWindowPos(consoleWindow,0,2000,0,0,0,SWP_NOSIZE);
+        consoleCreated = true;
+    }
+    static bool consoleCreated;
+    static FILE* fileIn;
+    static FILE* fileOut;
+    static FILE* fileErr;
+};
